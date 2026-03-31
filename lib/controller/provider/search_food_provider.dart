7dfe0ff4237e003f76c91c54bc8929f8b1f3eb7e
NFD Lix <felixsf123@gmail.com>
2026-03-31 20:52:28 +0700

    init

---
+++ lib/controller/provider/search_food_provider.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/foundation.dart';
+
+class SearchFoodProvider extends ChangeNotifier {
+  final String classification;
+  
+}
---
+++ lib/controller/provider/search_food_state.dart
@@ -0,0 +1,19 @@
+import 'package:food_recognizer_app/models/meal_response.dart';
+
+sealed class SearchFoodState {}
+
+final class SearchFoodInitial extends SearchFoodState {}
+
+final class SearchFoodLoading extends SearchFoodState {}
+
+final class SearchFoodSuccess extends SearchFoodState {
+  final List<MealsResponse> meals;
+
+  SearchFoodSuccess({required this.meals});
+}
+
+final class SearchFoodFailure extends SearchFoodState {
+  final String message;
+
+  SearchFoodFailure({required this.message});
+}
